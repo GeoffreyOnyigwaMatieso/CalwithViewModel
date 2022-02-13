48a32a375a79f15530a9249a55e5c738b9cf7315
Gmatieso <matiesogeoffrey@gmail.com>
2022-02-13 09:39:19 +0300

    setting views in the mainfile, setting onClickListener, validation check

--- app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
@@ -16,14 +16,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+    // binding the activity layout with main layout using findView by id
         num1EditText = findViewById(R.id.editTextNumberOne)
         num2EditText = findViewById(R.id.editTextNumberTwo)
         answerTextView = findViewById(R.id.textViewAnswer)
         calculateButton = findViewById(R.id.buttonCalculate)
+    // setting onClickListener on the calculateButton
+        calculateButton.setOnClickListener {
+            val num1 = num1EditText.text.toString()
+            val num2 = num2EditText.text.toString()
+
+            // validation check if EditText is empty
+        if(num1.isEmpty() || num2.isEmpty())
+        {
+            return@setOnClickListener
+        }
+
+        }
 
 
-
-
+    }
+    // fun calculate button
+    fun addTwoNumbers(num1:Int,num2:Int):Int {
+        return num1 + num2
     }
 }
