87d7faaba1e7e29c3cd16fe87aae8379825cec5c
Gmatieso <matiesogeoffrey@gmail.com>
2022-02-13 10:06:42 +0300

    created mutable live data and liveData

--- app/src/main/java/com/example/calwithviewmodel/mainViewModel.kt
+++ app/src/main/java/com/example/calwithviewmodel/mainViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.calwithviewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class mainViewModel : ViewModel() {
-
+    private val _result = MutableLiveData<Int> (0)
+    val result : LiveData<Int> = _result
+    
 
 }
