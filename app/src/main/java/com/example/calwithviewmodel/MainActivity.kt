ee8b3bb7ff1ce603d02d4079bebdd589bc2d27f8
Gmatieso <matiesogeoffrey@gmail.com>
2022-02-13 10:30:54 +0300

    creating an observer

--- app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
@@ -14,36 +15,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var calculateButton: Button
 
     // created viewModel variable
-    private lateinit var  viewModel: mainViewModel
+    private lateinit var viewModel: mainViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    // binding the activity layout with main layout using findView by id
+        // binding the activity layout with main layout using findView by id
         num1EditText = findViewById(R.id.editTextNumberOne)
         num2EditText = findViewById(R.id.editTextNumberTwo)
         answerTextView = findViewById(R.id.textViewAnswer)
         calculateButton = findViewById(R.id.buttonCalculate)
 
-    // made  main activity aware of viewModel class  using this oneliner provider code
-    viewModel = ViewModelProvider(this)[mainViewModel::class.java]
+        // made  main activity aware of viewModel class  using this oneliner provider code
+        viewModel = ViewModelProvider(this)[mainViewModel::class.java]
 
-    // setting onClickListener on the calculateButton
+        // setting onClickListener on the calculateButton
         calculateButton.setOnClickListener {
             val num1 = num1EditText.text.toString()
             val num2 = num2EditText.text.toString()
 
             // validation check if EditText is empty
-        if(num1.isEmpty() || num2.isEmpty())
-        {
-            return@setOnClickListener
-        }
-            
+            if (num1.isEmpty() || num2.isEmpty()) {
+                return@setOnClickListener
+            }
+            // calling addtwonumbers method from the viewmodel class
+            mainViewModel.addTwoNumbers(num1.toInt(), num2.toInt())
 
         }
-
+        mainViewModel.result.observe(this, Observer { result ->
+            answerTextView.text = result.toString()
+        })
 
     }
-
 }
