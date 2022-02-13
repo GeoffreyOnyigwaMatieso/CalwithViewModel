863cd4468e27b2b91b1143c9032461fa2dc0da1e
Gmatieso <matiesogeoffrey@gmail.com>
2022-02-13 09:21:56 +0300

    created variable, used findViewById to initialize them

--- app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/calwithviewmodel/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.calwithviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var num1EditText: EditText
+    private lateinit var num2EditText: EditText
+    private lateinit var answerTextView: TextView
+    private lateinit var calculateButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        num1EditText = findViewById(R.id.editTextNumberOne)
+        num2EditText = findViewById(R.id.editTextNumberTwo)
+        answerTextView = findViewById(R.id.textViewAnswer)
+        calculateButton = findViewById(R.id.buttonCalculate)
+
+
+
+
     }
 }
